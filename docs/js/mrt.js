8b144234c216cf968d2e92d834303a01cb74704d
Andrea Leoncini <andrea.leoncini@gmail.com>
2026-05-10 17:47:15 +0200

    action parameter

--- docs/js/mrt.js
+++ docs/js/mrt.js
@@ -9,6 +9,15 @@ function setAlert(msg) {
     }, 3000);
 };
 
+const generateHash = (string) => {
+  let hash = 0;
+  for (const char of string) {
+    hash = (hash << 5) - hash + char.charCodeAt(0);
+    hash |= 0; // Constrain to 32bit integer
+  }
+  return hash;
+};
+
 function resetReport() {
     var trips = [];
     localStorage.setItem('mrtTrips', JSON.stringify(trips));
